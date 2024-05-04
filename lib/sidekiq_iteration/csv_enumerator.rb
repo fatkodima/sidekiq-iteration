3b3dcad598149e75ce49101150be0bd02120979a
fatkodima <fatkodima123@gmail.com>
2024-05-04 23:51:10 +0300

    Check if `CSV` constant is loaded
    This moves any uninitialized constant error to the calling code,
    which hopefully makes it clear to devs that they should
    `require "csv"` before using the CsvEnumerator.

--- lib/sidekiq_iteration/csv_enumerator.rb
+++ lib/sidekiq_iteration/csv_enumerator.rb
@@ -36,7 +36,7 @@ module SidekiqIteration
     #   SidekiqIteration::CsvEnumerator.new(csv).rows(cursor: cursor)
     #
     def initialize(csv)
-      unless csv.instance_of?(CSV)
+      unless defined?(CSV) && csv.instance_of?(CSV)
         raise ArgumentError, "CsvEnumerator.new takes CSV object"
       end
 
